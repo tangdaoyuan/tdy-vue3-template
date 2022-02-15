6edaeb9f060db3b790608f2ebc7c1aea157bae57
唐道源 <tangdaoyuan@megvii.com>
2022-02-15 18:14:29 +0800

    feat: update dependencies & update element import

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,8 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import { registerElementUI } from './components/ElementUI/index';
-import 'element-plus/lib/theme-chalk/index.css';
 import './assets/styles/index.scss';
 
 const app = createApp(App);
-
-registerElementUI(app);
 
 app.use(store).use(router).mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const apiMocker = require('mocker-api');
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const mockerPriority = process.env.VUE_APP_MOCKER_PRIORITY || 'proxy';
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -37,6 +40,14 @@ module.exports = {
             alias: {
                 '@api': '@/http/api'
             }
-        }
+        },
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
     }
 };
